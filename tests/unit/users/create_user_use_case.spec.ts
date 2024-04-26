683cfd84fc86728760bd45078d418c9ad4b37b68
Marcos Daniel <marcosdaniel99@protonmail.com>
2024-04-25 23:49:23 -0300

    test: create user (use case)

---
+++ tests/unit/users/create_user_use_case.spec.ts
@@ -0,0 +1,25 @@
+import { test } from '@japa/runner'
+import { InMemoryUsersRepository } from '#repositories/in_memory/in_memory_users_repository'
+import { CreateUserUseCase } from '#use_cases/users/create_use_use_case'
+import { Prisma } from '@prisma/client'
+import hash from '@adonisjs/core/services/hash'
+
+test('create user', async ({ assert }) => {
+  const usersRepository = new InMemoryUsersRepository()
+  const sut = new CreateUserUseCase(usersRepository)
+
+  const userData: Prisma.UserCreateInput = {
+    name: 'User',
+    email: 'user123@email.com',
+    password_hash: '123456789',
+  }
+
+  await sut.execute({
+    data: userData,
+  })
+
+  const user = await usersRepository.findByEmail(userData.email)
+
+  assert.isTrue(await hash.verify(user!.password_hash, userData.password_hash))
+  assert.exists(user)
+})
