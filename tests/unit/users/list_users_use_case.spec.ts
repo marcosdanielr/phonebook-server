4787e987131cd36321012ad37aebf8a7d010955e
Marcos Daniel <marcosdaniel99@protonmail.com>
2024-04-25 23:49:07 -0300

    fix: list users use case test imports

--- tests/unit/users/list_users_use_case.spec.ts
+++ tests/unit/users/list_users_use_case.spec.ts
@@ -1,6 +1,6 @@
 import { test } from '@japa/runner'
-import { InMemoryUsersRepository } from '../../../app/repositories/in_memory/in_memory_users_repository.js'
-import { ListUsersUseCase } from '../../../app/use_cases/users/list_users_use_case.js'
+import { InMemoryUsersRepository } from '#repositories/in_memory/in_memory_users_repository'
+import { ListUsersUseCase } from '#use_cases/users/list_users_use_case'
 
 test('get all users', async ({ assert }) => {
   const usersRepository = new InMemoryUsersRepository()
