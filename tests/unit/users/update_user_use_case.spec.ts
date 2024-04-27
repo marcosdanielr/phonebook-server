96b6c0b79f222710a1694972c0e2d740f4b7fe33
Marcos Daniel <marcosdaniel99@protonmail.com>
2024-04-27 02:00:02 -0300

    test: update user (use case)

---
+++ tests/unit/users/update_user_use_case.spec.ts
@@ -0,0 +1,53 @@
+import { InMemoryUsersRepository } from '#repositories/in_memory/in_memory_users_repository'
+import { UserNotFoundError } from '#use_cases/errors/user_not_found_error'
+import { UpdateUserUseCase } from '#use_cases/users/update_user_use_case'
+import hash from '@adonisjs/core/services/hash'
+import { test } from '@japa/runner'
+
+let usersRepository: InMemoryUsersRepository
+let sut: UpdateUserUseCase
+
+test.group('Delete User Use Case', (group) => {
+  group.each.setup(async () => {
+    usersRepository = new InMemoryUsersRepository()
+    sut = new UpdateUserUseCase(usersRepository)
+
+    return () => {
+      usersRepository = undefined!
+      sut = undefined!
+    }
+  })
+
+  test('should be able delete user by id', async ({ assert }) => {
+    for (let i = 1; i <= 3; i++) {
+      await usersRepository.create({
+        name: `user-${i}`,
+        email: `user${i}@test.com`,
+        password_hash: await hash.make('123456789'),
+      })
+    }
+
+    await sut.execute({
+      id: 2,
+      data: {
+        name: 'Marcos',
+      },
+    })
+
+    const users = await usersRepository.list(1)
+
+    assert.include(users[1], {
+      id: 2,
+      name: 'Marcos',
+      password_hash: users[1].password_hash,
+    })
+  })
+
+  test("shouldn't be able delete user if not exists", async ({ assert }) => {
+    try {
+      await sut.execute({ id: 20, data: { name: 'Marcos' } })
+    } catch (error) {
+      assert.instanceOf(error, UserNotFoundError)
+    }
+  })
+})
