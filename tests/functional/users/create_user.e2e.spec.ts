7d59b16aa6c3c509756cb0f81425958674b362de
Marcos Daniel <marcosdaniel99@protonmail.com>
2024-04-27 23:23:47 -0300

    test: create user (e2e)

---
+++ tests/functional/users/create_user.e2e.spec.ts
@@ -0,0 +1,26 @@
+import { test } from '@japa/runner'
+
+test.group('Create User (E2E)', () => {
+  test('should be able create user', async ({ client }) => {
+    const authResponse = await client.post('/api/auth').json({
+      email: 'marcosadm@email.com',
+      password: '123456789',
+    })
+
+    const { access_token: accessToken } = authResponse.body()
+
+    const response = await client
+      .post('/api/users')
+      .json({
+        name: 'Testing',
+        role: 'USER',
+        email: 'testing123@email.com',
+        password: '123456789',
+      })
+      .headers({
+        Authorization: `Bearer ${accessToken}`,
+      })
+
+    response.assertStatus(201)
+  })
+})
